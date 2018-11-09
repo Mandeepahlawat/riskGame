cdfaae1b92afae9431c7e3cd7a9439bb0f311c25
Shreyas Movva <shreyasvmovva@gmail.com>
2018-11-09 13:21:53 -0500

    javadoc

--- src/Views/PhaseView.java
+++ src/Views/PhaseView.java
@@ -10,8 +10,24 @@ import Map.Map;
 import Player.Player;
 import Player.Player.GamePhase;
 
+/**
+ * This class will implement from observer
+ * and it displays player's name and different
+ * functionality of the phase's.
+ * 
+ * @author shreyas
+ * @version 1.0
+ * 
+ */
 public class PhaseView implements Observer {
 	
+	/**
+	 * This method will display player's name 
+	 * and also calculate's the reinforcement armies.
+	 * 
+	 * @param player creation of new player in the
+	 * game.
+	 */
 	public void reinforcementPhaseView(Player player) {
 		System.out.println("\n***********************"
 							+ player.getName()
@@ -19,6 +35,13 @@ public class PhaseView implements Observer {
 		player.placeReinforcements(player.calculateReinforcementArmies());
 	}
 	
+	/**
+	 * This method will display the player's 
+	 * name and also allow player to move his
+	 * armies to defend in player's class.
+	 * 
+	 * @param player Creation of player in game.
+	 */
 	public void fortificationPhaseView(Player player) {
 		System.out.println("\n***********************"
 							+ player.getName()
@@ -26,6 +49,13 @@ public class PhaseView implements Observer {
 		player.fortification();
 	}
 	
+	/**
+	 * This method will display the player's 
+	 * name and also allow player to move his
+	 * armies to attack in player's class.
+	 * 
+	 * @param player creation of player in game.
+	 */
 	public void attackPhaseView(Player player) {
 		System.out.println("\n***********************"
 							+ player.getName()
@@ -33,6 +63,12 @@ public class PhaseView implements Observer {
 		player.attack();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * This method will update the different phases 
+	 * only after passing the condition.
+	 * 
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		Player currentPlayer = ((Player) o);
