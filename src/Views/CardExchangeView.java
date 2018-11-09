0e3980891daec5863ca77fd0cc67dd7224b5b323
Shreyas Movva <shreyasvmovva@gmail.com>
2018-11-09 14:35:52 -0500

    Generate javadoc

--- src/Views/CardExchangeView.java
+++ src/Views/CardExchangeView.java
@@ -6,9 +6,28 @@ import java.util.Scanner;
 
 import Card.Card;
 import Player.Player;
+/**
+ * This method will implement from observer
+ * and also display's the Card exchange process 
+ * between the player's in the game.Updates when
+ * the observer notifies it's changes.
+ * 
+ * @author Mandeep Ahlawat
+ * @version 1.0
+ * @since 04/11/2018
+ *
+ */
 
 public class CardExchangeView implements Observer{
 	
+	/**
+	 * This method will display the selection of card's 
+	 * that can exchanged between the player's in the game.
+	 * 
+	 * @param currentPlayer object of player which 
+	 * display's current player details.
+	 * 
+	 */
 	public static void cardExchangeSelection(Player currentPlayer) {
 		System.out.println("You currently have following cards with you.");
 		int i = 1;
@@ -33,6 +52,13 @@ public class CardExchangeView implements Observer{
 				Integer.parseInt(cardnums[3]));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * This method print's out start and end 
+	 * of card exchange map and display's
+	 * the current player's card's after update
+	 * method is called.
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		Player currentPlayer = (Player) o;
