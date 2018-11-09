6794a9d368a86b5d55480293029fc6f946e1395c
Shreyas Movva <shreyasvmovva@gmail.com>
2018-11-08 21:26:19 -0500

    java doc

--- src/Card/Card.java
+++ src/Card/Card.java
@@ -10,6 +10,7 @@ import Player.Player;
  * This class is the Card class and it has CardType 
  * in which three different types of army cards are  
  * are mentioned.
+ * 
  * @author Mandeep Ahlawat
  * @version 1.0
  * @since 2018/10/27
@@ -18,17 +19,50 @@ import Player.Player;
 
 public class Card {
 	public enum CardType {
-		INFANTRY, CAVALRY, ARTILLERY;
+		/**
+		 * It is a branch of a army that engages 
+		 * in military combat on foot.
+		 */
+		INFANTRY,
+		/**
+		 * It is a branch of army that engages in
+		 * fight mounted on horseback.
+		 */
+		CAVALRY,
+		/**
+		 * It is branch of army that engages in fight 
+		 * with havy military weapons. 
+		 */
+		ARTILLERY;
 		
+		/**
+		 * Static method to create a object 
+		 * The name of the method is CardType 
+		 * to get a random card
+		 * 
+		 * @return a values which is defined in the method.
+		 */
 		public static CardType getRandomCard() {
 	        Random random = new Random();
 	        return values()[random.nextInt(values().length)];
 	    }
 	}
-	
+	/**
+	 * Type of the card
+	 */
 	public CardType type;
+	/**
+	 * cardExchangeValue is intialized to 5
+	 */
 	public static int cardExchangeValue = 5;
+	/**
+	 * territory in the game
+	 */
 	public Territory territory;
+	/**
+	 * particular owner in the game for 
+	 * the territory
+	 */
 	public Player owner;
 	
 	/**
