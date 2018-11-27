00d84acfa288837fac00239c2e51c2e5c1d35060
Shreyas Movva <shreyasvmovva@gmail.com>
2018-11-27 14:03:08 -0500

    Generate javadoc

--- src/Player/Strategy.java
+++ src/Player/Strategy.java
@@ -1,35 +1,41 @@
 package Player;
 /**
- * The classes what should implement this interface
- * should define attack, reinforcement
- * and fotify methods
+ * The interface which defines attack, reinforcement
+ * and fotify methods.
+ * 
+ * The classes will implement this interface.
  * 
  * @author mandeepahlawat
+ * @version 1.0
+ * @since 24-11-2018
  *
  */
 public interface Strategy {
 	/**
 	 * Calculation of reinforcement armies,
-	 * which will vary depending on the strategy
+	 * which will vary depending on the strategy.
+	 * 
 	 * @return int value of the reinforcement armies
+	 * 
 	 */
 	public int calculateReinforcementArmies();
 	/**
 	 * Placement of reinforcement armies,
-	 * which will vary depending on the strategy
+	 * which will vary depending on the strategy.
 	 * 
 	 * @param reinforcements - number of reinforcements
-	 * to be place
+	 * to be place.
 	 */
 	public void placeReinforcements(int reinforcements);
 	/**
 	 * Fortification phase, whose implementation will
-	 * vary depending on the strategy
+	 * vary depending on the strategy.
+	 * 
 	 */
 	public void fortification();
 	/**
 	 * Attack phase, whose implementation will
-	 * vary depending on the strategy
+	 * vary depending on the strategy.
 	 */
 	public void attack();
 }
